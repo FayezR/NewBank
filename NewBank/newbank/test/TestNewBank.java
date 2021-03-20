b0a0134904f2cae34f57e66115bd22c9da5f619d
ap2881 <ap2881@bath.ac.uk>
2021-03-20 23:23:02 +0000

    Unit Test for payment added

---
+++ NewBank/newbank/test/TestNewBank.java
@@ -0,0 +1,26 @@
+package newbank.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import newbank.server.CustomerID;
+import newbank.server.NewBank;
+
+class TestNewBank {
+	
+	private NewBank bank;
+	
+	@Test
+	void testProcessRequest() {
+		bank = NewBank.getBank();
+		CustomerID customer = bank.checkLogInDetails("Bhagy", "bhagy");
+		String [] request = new String [] {"PAY", "FROM", "Main", "TO", "John", "Checking", "100"};
+		String output =  bank.processRequest(customer, request);
+		//String expectedOutput = "SUCCESS - " + "Your account balance:" + "\n" + showMyAccounts(customer);
+			
+		assertEquals(output.contains("SUCCESS"), true); // show success
+		assertEquals(output.contains("900"), true); //show remaining balance
+	}
+
+}
