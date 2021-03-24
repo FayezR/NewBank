862f8f3da889cd6a0fca41f96248ceb5ea6ff3e3
raymo <rt778@bath.ac.uk>
2021-03-24 19:35:27 +0100

    Retry

---
+++ NewBank/newbank/server/Menu.java
@@ -0,0 +1,20 @@
+package newbank.server;
+
+public class Menu {
+	
+	
+public static String printMenu() {
+	String menu = "Welcome to NewBank. The Disruptive Innovative Bank.\n"
+			+ "Selection Menu:\n"
+			+ "1- Check your accounts balances.\n"
+			+ "2- Create new account.\n"
+			+ "3- Pay counterparties.\n"
+			+ "4- Offer a Micro-Loan.\n"
+			+ "5- View Micro-Loans Available\n"
+			+ "6- Acquire a Micro-Loan.\n"
+			+ "(Select a number or type \"MENU\" to see the Menu again).\n";
+	return menu;
+}
+	
+
+}
---
+++ NewBank/newbank/server/Microloan.java
@@ -0,0 +1,16 @@
+package newbank.server;
+
+public class Microloan {
+	
+	
+	
+	//methods
+	
+	public String offerLoan() {
+		
+		String offer = "Offer a Micro-Loan";
+		
+		return offer;
+	}
+
+}
