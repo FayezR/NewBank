4ef9b6d708e376effaa12110535b78681c458e03
ExtremeC0der <64635876+ExtremeC0der@users.noreply.github.com>
2021-04-08 19:29:29 +0100

    Added Logout functionality for admins and customers

--- NewBank/newbank/server/Menu.java
+++ NewBank/newbank/server/Menu.java
@@ -14,14 +14,16 @@ public class Menu {
 				+ "6- Activate the MicroLoan (meaning putting the MicroLoan account in funds).\n"
 				+ "7- Show MicroLoans Available.\n"
 				+ "8- Acquire a Micro-Loan.\n"
-				+ "9- Show full transaction history.\n";
+				+ "9- Show full transaction history.\n"
+				+ "11- Logout";
 		return menu;
 	}
 	
 	public static String printAdminMenu(){
 		String menu = "Welcome admin!"
 				+ "Selection Menu:\n"
-				+ "1 - Add a new customer \n";
+				+ "1 - Add a new customer \n"
+				+ "2 - Logout";
 
 		return menu;
 	}
